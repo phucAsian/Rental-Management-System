b84375207b27db4196da2d9eae7cbb13ab2103aa
phuc <le30426@gmail.com>
2026-04-10 02:48:44 +0700

    UPDATE: Fix minor error

--- src/factories/userFactory.js
+++ src/factories/userFactory.js
@@ -1,34 +1,37 @@
-class Tenant {
-  constructor(data) {
+const bcrypt = require('bcryptjs');
+
+class User {
+  constructor(data, role) {
     this.full_name = data.full_name;
     this.email = data.email;
     this.phone = data.phone;
-    this.password_hash = data.password;
+    
+    if (data.password) {
+      this.password_hash = bcrypt.hashSync(data.password, 8);
+    } else {
+      this.password_hash = data.password_hash || ""; 
+    }
 
     this.id_card = data.id_card;
     this.dob = data.dob;
     this.hometown = data.hometown;
     this.gender = data.gender;
     this.avatar_url = data.avatar_url || "/img/default-avatar.png";
-    this.role = "Tenant";
+    this.role = role;
     this.is_active = true;
   }
 }
 
-class Admin {
-  constructor(data) {
-    this.full_name = data.full_name;
-    this.email = data.email;
-    this.phone = data.phone;
-    this.password_hash = data.password;
 
-    this.id_card = data.id_card;
-    this.dob = data.dob;
-    this.hometown = data.hometown;
-    this.gender = data.gender;
-    this.avatar_url = data.avatar_url || "/img/default-avatar.png";
-    this.role = "Admin";
-    this.is_active = true;
+class Tenant extends User {
+  constructor(data) {
+    super(data, "Tenant"); 
+  }
+}
+
+class Admin extends User {
+  constructor(data) {
+    super(data, "Admin");
   }
 }
 
